907f9ecd44136913c56552829502dfa343dc6301
snovosel <novosel991@gmail.com>
2018-12-30 00:45:59 -0500

    fixed importing of User model in user controller

--- dist/controllers/Users.js
+++ dist/controllers/Users.js
@@ -1,37 +1,30 @@
-'use strict';
+"use strict";
 
-var _sequelize = require('sequelize');
-
-var _sequelize2 = _interopRequireDefault(_sequelize);
-
-var _Users = require('../models/Users.js');
-
-var _Users2 = _interopRequireDefault(_Users);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var sequelize = new _sequelize2.default('dogbook', 'snovosel', 'Flwr1281!', {
-  dialect: 'postgresql',
-  host: "localhost",
-  port: 5432,
-  define: {
-    timestamps: false
-  }
-});
-
-var Users = (0, _Users2.default)(sequelize, _sequelize2.default);
+var _index = require("../models/index.js");
 
 exports.getAllUsers = function (req, res) {
-  Users.findAll().then(function (users) {
+  _index.Users.findAll().then(function (users) {
     if (users.length === 0) {
       res.send({ "users": "no sir bob" });
     }
     res.send({ "users": users });
   });
-};
+}; // import Sequelize from 'sequelize';
+// import UsersModel from '../models/users.js';
+
+// const sequelize = new Sequelize('dogbook', 'snovosel', 'Flwr1281!', {
+//   dialect: 'postgresql',
+//   host: "localhost",
+//   port: 5432,
+//   define: {
+//     timestamps: false
+//   }
+// });
+
+// const Users = UsersModel(sequelize, Sequelize);
 
 exports.createUser = function (req, res) {
-  Users.findOrCreate({ where: { email: req.body.email } }).spread(function (user, created) {
+  _index.Users.findOrCreate({ where: { email: req.body.email } }).spread(function (user, created) {
     var userResponse = user.get({ plan: true });
 
     if (created === true) {
